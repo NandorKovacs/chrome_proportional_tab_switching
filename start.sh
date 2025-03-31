8ee9376947b841acb3dc3ca2453109ee7b83a10e
NandorKovacs <nandor1kovacs@gmail.com>
2025-03-31 22:17:44 +0200

    icons are pngs now

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 chromium --user-data-dir=test_chromium \
          --no-first-run \
-         --load-extension=dist
-	 --enable-logging=stderr \
+         --load-extension=dist \
+	       --enable-logging=stderr \
          --v=1
